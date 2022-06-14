3a3822acb0e463d0f75ea68e476dcc3a0b0f7838
apoorvnv <104858693+apoorvnv@users.noreply.github.com>
2022-06-15 00:32:37 +0530

    Create asc_and_desc.py

---
+++ Practical3/asc_and_desc.py
@@ -0,0 +1,10 @@
+# Apoorv Varkute 
+# ascending and descending in array
+
+h=list(map(int,input("Enter array elements: ").split()))
+print("Array elements in Ascending order: ")
+h.sort()
+print(*h)
+h.sort(reverse=True)
+print("Array elements in Descending order: ")
+print(*h)
