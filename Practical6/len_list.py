4cd19eb59e7b1ffc4a625d2b1f09756fe0472024
apoorvnv <104858693+apoorvnv@users.noreply.github.com>
2022-07-09 20:59:12 +0530

    Add files via upload

---
+++ Practical6/len_list.py
@@ -0,0 +1,13 @@
+# Apoorv Varkute
+# find length of string using recurssion
+
+def av(t):
+    if(t == ''):
+        return 0
+    else:
+        return 1 + av(t[1:])
+
+t=(input("Enter string: "))
+print("length of string is ")
+x = av(t)
+print(x)
